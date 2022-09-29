cf76e6aab287e01988d4b041d67be2d136d123dc
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2022-09-29 18:33:53 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,17 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  let boolean = true;
+  const animalFilter = species.find((element) => element.name === animal);
+  animalFilter.residents.forEach((resident) => {
+    if (resident.age < age) {
+      boolean = false;
+    }
+  });
+  return boolean;
 }
+
+console.log(getAnimalsOlderThan('otters', 52245));
 
 module.exports = getAnimalsOlderThan;
