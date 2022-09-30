6e4905e1234f69e45343e2e6f1eaa8da6b3407b3
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2022-09-30 10:02:59 -0300

    Requisitos 6 e 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,45 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('caso receba o parâmetro count, é necessário retornar a quantidade de elefantes', () => {
+    const actual = handlerElephants('count');
+    const expected = 4;
+    expect(actual).toEqual(expected);
+  });
+
+  it('caso receba o parâmetro names, é necessário retornar um array de nomes', () => {
+    const actual = handlerElephants('names');
+    const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+    expect(actual).toEqual(expected);
+  });
+
+  it('caso receba o parâmetro averageAge, é necessário retornar a média de idade dos elefantes', () => {
+    const actual = handlerElephants('averageAge');
+    const expected = 10.5;
+    expect(actual).toEqual(expected);
+  });
+
+  it('caso receba o parâmetro location, é necessário retornar a localização dos elefantes dentro do Zoológico', () => {
+    const actual = handlerElephants('location');
+    const expected = 'NW';
+    expect(actual).toEqual(expected);
+  });
+
+  it('caso receba o parâmetro popularity, é necessário retornar a popularidade dos elefantes', () => {
+    const actual = handlerElephants('popularity');
+    const expected = 5;
+    expect(actual).toEqual(expected);
+  });
+
+  it('caso receba o parâmetro availability, é necessário retornar um array com a relação de dias em que é possível visitar os elefantes', () => {
+    const actual = handlerElephants('availability');
+    const expected = ['Friday', 'Saturday', 'Sunday', 'Tuesday'];
+    expect(actual).toEqual(expected);
+  });
+
+  it('caso não receba um parâmetro, é necessário retornar undefined', () => {
+    const actual = handlerElephants();
+    const expected = undefined;
+    expect(actual).toEqual(expected);
+  });
+});
