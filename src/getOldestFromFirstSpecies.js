ce17b45ad7df4b3ecfff004bd3f4f3dc2575efcb
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2022-10-03 12:19:52 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,8 +1,12 @@
 const data = require('../data/zoo_data');
-// const { species } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const resp = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const oldestAnimal = species.find((specie) => specie.id === resp)
+    .residents.sort((a, b) => b.age - a.age)[0];
+  return Object.values(oldestAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
