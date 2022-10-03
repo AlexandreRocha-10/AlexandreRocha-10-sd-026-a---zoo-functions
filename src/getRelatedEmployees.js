f7058fee5e9a0a538a6c0931be353c357129eae5
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2022-10-03 10:56:50 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,13 +1,20 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  if (id) {
-    return true;
-  } return false;
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
+console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const employeesList = [];
+  if (isManager(managerId)) {
+    employees.map((employee) => employeesList.push(`${employee.firstName} ${employee.lastName}`));
+  } else {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employeesList;
 }
 
 module.exports = { isManager, getRelatedEmployees };
