1371aa9dc74cc23d3ea28af67b27d244c563fbfa
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2022-10-03 12:03:18 -0300

    Requisito 4 ok

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -5,12 +5,11 @@ function isManager(id) {
   return employees.some((employee) => employee.managers.includes(id));
 }
 
-console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
-
 function getRelatedEmployees(managerId) {
   const employeesList = [];
   if (isManager(managerId)) {
-    employees.map((employee) => employeesList.push(`${employee.firstName} ${employee.lastName}`));
+    const subord = employees.filter((emp) => emp.managers.includes(managerId));
+    subord.map((employee) => employeesList.push(`${employee.firstName} ${employee.lastName}`));
   } else {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
